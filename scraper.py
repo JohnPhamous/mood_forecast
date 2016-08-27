9327280039fd9b64bd0bc8e45931536118547af1
John Pham <johnphammail@gmail.com>
2016-08-27 12:47:48 -0700

    added scraper

---
+++ scraper.py
@@ -0,0 +1,38 @@
+# Number of tweets per hour throughout the day
+# Mood of the tweet
+# Base off what's trending from twitter
+# TODO: Look into Algorithmia and AlchemyAPI
+
+from __future__ import absolute_import, print_function
+
+import tweepy, json, re
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+
+# Twitter Developer credentials
+API_KEY = "UoxZJjMB30XE9ox7PBZe1qqbZ"
+API_SECRET = "mDiSSt9IWQFn8IdZAHE8IeYbyqSmIPoN4hI80ezk2JHl0XVazp"
+ACCESS_TOKEN = "115831938-Nfbk74K0xheWpWyGN4Cl2Vs9shcRl9CJ3gUdwZYV"
+ACCESS_TOKEN_SECRET = "rmPh9burqeUOvzcvE1T2pkQAzkuN3bVxjfUnH4mfi4M2J"
+
+# Locations
+galvinize = [37.787601, -122.396643]
+
+class StdOutListener(StreamListener):
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+if __name__ == '__main__':
+    listener = StdOutListener()
+    # Authenticates with Twitter
+    auth = OAuthHandler(API_KEY, API_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    stream = Stream(auth, listener)
+    stream.filter(locations = LOCATION)
+    # stream.filter(track = ['test'])
