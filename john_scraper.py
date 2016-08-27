662b61e5aa2f1b027b9515427eaeb009ae1832ca
John Pham <johnphammail@gmail.com>
2016-08-27 15:50:37 -0700

    fixed removeNonsense

--- john_scraper.py
+++ john_scraper.py
@@ -10,6 +10,10 @@ from tweepy import Stream
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 
+from googleapiclient import discovery
+import httplib2
+from oauth2client.client import GoogleCredentials
+
 # Twitter Developer credentials
 API_KEY = "UoxZJjMB30XE9ox7PBZe1qqbZ"
 API_SECRET = "mDiSSt9IWQFn8IdZAHE8IeYbyqSmIPoN4hI80ezk2JHl0XVazp"
@@ -18,7 +22,6 @@ ACCESS_TOKEN_SECRET = "rmPh9burqeUOvzcvE1T2pkQAzkuN3bVxjfUnH4mfi4M2J"
 
 # Locations
 galvinize = [-122.451665,37.757656,-122.364925,37.80439]
-data_json = None
 
 def removeNonsense(data_json):
     original_data_json = data_json
@@ -27,15 +30,16 @@ def removeNonsense(data_json):
     # Removes URLs
     data_json = re.sub(r'http\S+', '', data_json)
     # Removes mentions
-    data_json = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z\t])|(\w+:\/\/\S+)"," ",data_json).split())
-    print(data_json)
+    data_json = ' '.join(re.sub("(@[A-Za-z0-9]+)|(\w+:\/\/\S+)"," ",data_json).split())
+    # data_json = ' '.join(re.sub("(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+) "," ",data_json).split())
+    return data_json
 
 class StdOutListener(StreamListener):
     def on_data(self, data):
-        # print(data)
         data_json = json.loads(data)
-        # print("\n", data_json["text"])
-        removeNonsense(data_json["text"])
+        tweet_text = removeNonsense(data_json["text"])
+        tweet_date = data_json["created_at"][11:19]
+        print(tweet_date, tweet_text)
         return True
 
     def on_error(self, status):
@@ -50,4 +54,3 @@ if __name__ == '__main__':
     stream = Stream(auth, listener)
     stream.filter(locations = galvinize)
     # stream.filter(track = ['test'])
-
