f7ccfce786d5fa13f4eeaa4e1c457c5f4d52901c
Jarren San Jose <sanjose.jarren@gmail.com>
2016-08-27 16:38:09 -0700

    update

--- john_scraper.py
+++ john_scraper.py
@@ -14,6 +14,12 @@ ACCESS_TOKEN_SECRET = "rmPh9burqeUOvzcvE1T2pkQAzkuN3bVxjfUnH4mfi4M2J"
 galvinize = [-122.451665,37.757656,-122.364925,37.80439]
 
 tweet_text = None
+emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
 
 def removeNonsense(data_json):
     original_data_json = data_json
@@ -23,6 +29,8 @@ def removeNonsense(data_json):
     data_json = re.sub(r'http\S+', '', data_json)
     # Removes mentions
     data_json = ' '.join(re.sub("(@[A-Za-z0-9]+)|(\w+:\/\/\S+)"," ",data_json).split())
+    data_json = emoji_pattern.sub(r'',tweet_text)
+    data_json = re.sub(r''.tweet_text)
     # data_json = ' '.join(re.sub("(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+) "," ",data_json).split())
     return data_json
 
@@ -32,14 +40,7 @@ class StdOutListener(StreamListener):
         data_json = json.loads(data)
         tweet_text = removeNonsense(data_json["text"])
         tweet_date = data_json["created_at"][11:19]
-        emoji_pattern = re.compile("["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           "]+", flags=re.UNICODE)
-        print(tweet_date, emoji_pattern.sub(r'',tweet_text))
-        #print(tweet_date, tweet_text)
+        print(tweet_date, tweet_text)
 
         #print(tweet_date, tweet_text)
 
